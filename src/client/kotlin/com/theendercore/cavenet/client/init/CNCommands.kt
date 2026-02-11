b723f7adb9183cc5674148775b3c4a83fc8b9eae
TheEnderCore <theendercore@gmail.com>
2026-02-11 17:44:03 +0200

    freeze / unfreeze all

--- src/client/kotlin/com/theendercore/cavenet/client/init/CNCommands.kt
+++ src/client/kotlin/com/theendercore/cavenet/client/init/CNCommands.kt
@@ -13,69 +13,86 @@ import net.minecraft.network.chat.Component
 import org.teamvoided.creative_works.comands.utils.ImprovedLookup.listSuggestions
 
 object CNCommands {
-    fun init() {
-        ClientCommandRegistrationCallback.EVENT.register { dispatcher, _ ->
-            val root = literal("cavenet").build()
-            dispatcher.root.addChild(root)
+    fun init() = ClientCommandRegistrationCallback.EVENT.register { dispatcher, _ ->
+        val root = literal("cavenet").build()
+        dispatcher.root.addChild(root)
 
-            val create = literal("create").executes(::create).build()
-            root.addChild(create)
+        val create = literal("create").executes(::create).build()
+        root.addChild(create)
 
-            val clear = literal("clear").executes(::clearNodes).build()
-            root.addChild(clear)
+        val clear = literal("clear").executes(::clearNodes).build()
+        root.addChild(clear)
 
-            val ticks = literal("ticks").executes {
-                it.source.sendFeedback(Component.translatable("Current tick rate is: ${CNNetworkManager.ticksToSkip}"))
-                1
-            }.build()
-            root.addChild(ticks)
+        val ticks = literal("ticks").executes {
+            it.source.sendFeedback(Component.translatable("Current tick rate is: ${CNNetworkManager.ticksToSkip}"))
+            1
+        }.build()
+        root.addChild(ticks)
 
-            val ticksArg = argument("ticks", IntegerArgumentType.integer(-1)).executes {
-                CNNetworkManager.ticksToSkip = IntegerArgumentType.getInteger(it, "ticks")
-                it.source.sendFeedback(Component.translatable("Tick rate set to: ${CNNetworkManager.ticksToSkip}"))
+        val ticksArg = argument("ticks", IntegerArgumentType.integer(-1)).executes {
+            CNNetworkManager.ticksToSkip = IntegerArgumentType.getInteger(it, "ticks")
+            it.source.sendFeedback(Component.translatable("Tick rate set to: ${CNNetworkManager.ticksToSkip}"))
 
-                0
-            }.build()
-            ticks.addChild(ticksArg)
+            0
+        }.build()
+        ticks.addChild(ticksArg)
 
 
-            val freeze = literal("freeze").then(
-                argument("id", StringArgumentType.string())
-                    .suggests { _, builder -> builder.listSuggestions(CNNetworkManager.networks.map { it.id.toString() }) }
-                    .executes { ctx ->
-                        val id = StringArgumentType.getString(ctx, "id")
-                        val net = CNNetworkManager.networks.firstOrNull { it.id.toString() == id }
-                        if (net != null) {
-                            net.phase = CaveNetwork.NetPhase.FROZEN
-                            ctx.source.sendFeedback(Component.translatable("Froze network: $id"))
-                            1
-                        } else {
-                            ctx.source.sendError(Component.translatable("No network with id: $id"))
-                            0
-                        }
+        val freeze = literal("freeze").then(
+            argument("id", StringArgumentType.string())
+                .suggests { _, builder -> builder.listSuggestions(CNNetworkManager.networks.map { it.id.toString() }) }
+                .executes { ctx ->
+                    val id = StringArgumentType.getString(ctx, "id")
+                    val net = CNNetworkManager.networks.firstOrNull { it.id.toString() == id }
+                    if (net != null) {
+                        net.phase = CaveNetwork.NetPhase.FROZEN
+                        ctx.source.sendFeedback(Component.translatable("Froze network: $id"))
+                        1
+                    } else {
+                        ctx.source.sendError(Component.translatable("No network with id: $id"))
+                        0
                     }
-            ).build()
-            root.addChild(freeze)
+                }
+        ).build()
+        root.addChild(freeze)
 
-            val unfreeze = literal("unfreeze").then(
-                argument("id", StringArgumentType.string())
-                    .suggests { _, builder -> builder.listSuggestions(CNNetworkManager.networks.map { it.id.toString() }) }
-                    .executes { ctx ->
-                        val id = StringArgumentType.getString(ctx, "id")
-                        val net = CNNetworkManager.networks.firstOrNull { it.id.toString() == id }
-                        if (net != null) {
-                            net.phase = CaveNetwork.NetPhase.OPENING_DOOR
-                            ctx.source.sendFeedback(Component.translatable("Unfroze network: $id"))
-                            1
-                        } else {
-                            ctx.source.sendError(Component.translatable("No network with id: $id"))
-                            0
-                        }
+
+        val unfreeze = literal("unfreeze").then(
+            argument("id", StringArgumentType.string())
+                .suggests { _, builder -> builder.listSuggestions(CNNetworkManager.networks.map { it.id.toString() }) }
+                .executes { ctx ->
+                    val id = StringArgumentType.getString(ctx, "id")
+                    val net = CNNetworkManager.networks.firstOrNull { it.id.toString() == id }
+                    if (net != null) {
+                        net.phase = CaveNetwork.NetPhase.OPENING_DOOR
+                        ctx.source.sendFeedback(Component.translatable("Unfroze network: $id"))
+                        1
+                    } else {
+                        ctx.source.sendError(Component.translatable("No network with id: $id"))
+                        0
                     }
-            ).build()
-            root.addChild(unfreeze)
-        }
+                }
+        ).build()
+        root.addChild(unfreeze)
 
+
+        val freezeAll = literal("freeze_all").executes { ctx ->
+            for (net in CNNetworkManager.networks) {
+                net.phase = CaveNetwork.NetPhase.FROZEN
+            }
+            ctx.source.sendFeedback(Component.translatable("Froze ${CNNetworkManager.networks} networks"))
+            0
+        }.build()
+        root.addChild(freezeAll)
+
+        val unfreezeAll = literal("unfreeze_all").executes { ctx ->
+            for (net in CNNetworkManager.networks) {
+                net.phase = CaveNetwork.NetPhase.OPENING_DOOR
+            }
+            ctx.source.sendFeedback(Component.translatable("Unfroze ${CNNetworkManager.networks} networks"))
+            0
+        }.build()
+        root.addChild(unfreezeAll)
     }
 
     fun create(ctx: CommandContext<FabricClientCommandSource>): Int {
